ad86fc6e2a0add93ec58ad7f54839b6533e2797d
jinwoong <baseball0020@gmail.com>
2024-04-29 18:38:15 +0900

    chore: first init

---
+++ Team0418Pokemon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Team0418Pokemon
+//
+//  Created by jinwoong Kim on 4/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Team0418Pokemon/Team0418PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team0418PokemonApp.swift
+//  Team0418Pokemon
+//
+//  Created by jinwoong Kim on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Team0418PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
