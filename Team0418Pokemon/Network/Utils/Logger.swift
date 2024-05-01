7f2d08c6c4c6de7bbae8620f26bb37317b2311c2
jinwoong <baseball0020@gmail.com>
2024-05-01 14:43:46 +0900

    feat: configure logging system

---
+++ Team0418Pokemon/Network/Utils/Logger.swift
@@ -0,0 +1,41 @@
+//
+//  Logger.swift
+//  Team0418Pokemon
+//
+//  Created by jinwoong Kim on 5/1/24.
+//
+
+import Foundation
+
+struct Logger {
+    enum Level: Int, CustomStringConvertible {
+        case error
+        case warn
+        case debug
+        
+        var description: String {
+            switch self {
+                case .error:
+                    return "‼️ ERROR:"
+                case .warn:
+                    return "⚠️ WARNING:"
+                case .debug:
+                    return "⚒️ DEBUG:"
+            }
+        }
+    }
+    
+    static let errorLog = log(level: .error)
+    static let debugLog = log(level: .debug)
+    static let warningLog = log(level: .warn)
+    
+    static func log(
+        level: Level
+    ) -> (String) -> ((String) -> String) {
+        return { name in
+            return { message in
+                "\(level)"
+            }
+        }
+    }
+}
