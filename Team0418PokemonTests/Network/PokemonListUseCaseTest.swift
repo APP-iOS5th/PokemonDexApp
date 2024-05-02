5696feb1600d7a1fcfd9ca00ce1d270ef131ee65
jinwoong <baseball0020@gmail.com>
2024-05-02 14:24:15 +0900

    test: test `ListUseCase` method `request(with:)`

--- Team0418PokemonTests/Network/PokemonListUseCaseTest.swift
+++ Team0418PokemonTests/Network/PokemonListUseCaseTest.swift
@@ -6,30 +6,57 @@
 //
 
 import XCTest
+@testable import Team0418Pokemon
 
 final class PokemonListUseCaseTest: XCTestCase {
+    var apiService: (any ListUseCase)!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [MockURLSessionProtocol.self]
+        let urlSession = URLSession(configuration: configuration)
+        
+        apiService = PokemonAPIService(session: urlSession)
+    }
+    
+    func test_request() async throws {
+        guard let path = Bundle(for: Team0418PokemonTests.self).url(forResource: "pokemonlist", withExtension: "json"),
+              let pokemonListData = try? Data(contentsOf: path) else {
+            XCTFail("Failed to get the static Pokemon List File.")
+            return
+        }
+        /// Since this json file is too large,
+        /// we cannot make data variable.
+        /// Instead this approach, I decide to use real network data.
+//        guard let path = Bundle(for: Team0418PokemonTests.self).url(forResource: "pokemondetail", withExtension: "json"),
+//              let pokemonDetailData = try? Data(contentsOf: path) else {
+//            XCTFail("Failed to get the static Pokemon List File.")
+//            return
+//        }
+        let (pokemonDetailData, _) = try await URLSession.shared.data(from: URL(string: "https://pokeapi.co/api/v2/pokemon/1")!)
+        
+        let offset = 0
+        let limit = 151
+        let urlString = "https://pokeapi.co/api/v2/pokemon/?limit=\(limit)&offset=\(offset)"
+        
+        MockURLSessionProtocol.loadingHandler = { request in
+            if request.description == urlString {
+                let response = HTTPURLResponse(url: URL(string: urlString)!, statusCode: 200, httpVersion: nil, headerFields: nil)
+                return (response!, pokemonListData)
+            } else {
+                let response = HTTPURLResponse(url: URL(string: urlString)!, statusCode: 200, httpVersion: nil, headerFields: nil)
+                return (response!, pokemonDetailData)
+            }
+        }
+        
+        
+        // result
+        let networkingResult = await apiService.request(with: 0, 151)
+        
+        XCTAssert(networkingResult.count == limit - offset)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        apiService = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
