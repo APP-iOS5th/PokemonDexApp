3b33b844fa35cbcb665da65986b9f2c68112eac2
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-03 13:22:13 +0900

    Apple Login완성

--- Team0418Pokemon/AppleLoginView.swift
+++ Team0418Pokemon/AppleLoginView.swift
@@ -8,49 +8,79 @@
 import SwiftUI
 import AuthenticationServices
 
-//struct AppleLoginView: View {
-//    var body: some View {
-//        Image("pokemonGen1BG 1")
-//            .resizable()
-//        VStack {
-//            ApplSignButton()
-//        }
-//
-//    }
-//}
-
 struct AppleLoginView: View {
+    @State private var isLogin = false
     var body: some View {
         ZStack {
             Image("pokemonGen1BG 1")
                 .resizable()
-            SignInWithAppleButton(
-                onRequest: { request in
-                    request.requestedScopes = [.fullName, .email]
-                },
-                onCompletion: { result in
-                    switch result {
-                    case .success(let authResults):
-                        print("Apple Login Success")
-                        switch authResults.credential {
-                        case let appleIDCredential as ASAuthorizationAppleIDCredential:
-                            //계정정보 가져오기
-                            _ = appleIDCredential.user
-                            let fullName = appleIDCredential.fullName
-                            let name = (fullName?.familyName ?? "") + (fullName?.givenName ?? "")
-                            let email = appleIDCredential.email
-                            let token = String(data: appleIDCredential.identityToken!, encoding: .utf8)
-                            let AuthorizationCode = String(data: appleIDCredential.authorizationCode!, encoding: .utf8)
-                        default:
-                            break
-                        }
-                    case .failure(let error):
-                        print(error.localizedDescription)
-                        print("error")                }
+            NavigationStack {
+                if isLogin {
+                    NavigationLink(destination: DetailView()) {
+                        SignInWithAppleButton(
+                            onRequest: { request in
+                                request.requestedScopes = [.fullName, .email]
+                            },
+                            onCompletion: { result in
+                                switch result {
+                                case .success(let authResults):
+                                    print("Apple Login Success")
+                                    switch authResults.credential {
+                                    case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                                        //계정정보 가져오기
+                                        _ = appleIDCredential.user
+                                        let fullName = appleIDCredential.fullName
+                                        let name = (fullName?.familyName ?? "") + (fullName?.givenName ?? "")
+                                        let email = appleIDCredential.email
+                                        let token = String(data: appleIDCredential.identityToken!, encoding: .utf8)
+                                        let AuthorizationCode = String(data: appleIDCredential.authorizationCode!, encoding: .utf8)
+                                        self.isLogin = true
+                                    default:
+                                        break
+                                    }
+                                case .failure(let error):
+                                    print(error.localizedDescription)
+                                    print("error")
+                                }
+                            }
+                        )
+                        .frame(width : UIScreen.main.bounds.width * 0.9, height:50)
+                        .cornerRadius(5)
+                    }
                 }
-            )
-            .frame(width : UIScreen.main.bounds.width * 0.9, height:50)
-            .cornerRadius(5)
+                else {
+                    SignInWithAppleButton(
+                        onRequest: { request in
+                            request.requestedScopes = [.fullName, .email]
+                        },
+                        onCompletion: { result in
+                            switch result {
+                            case .success(let authResults):
+                                print("Apple Login Success")
+                                switch authResults.credential {
+                                case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                                    //계정정보 가져오기
+                                    _ = appleIDCredential.user
+                                    let fullName = appleIDCredential.fullName
+                                    let name = (fullName?.familyName ?? "") + (fullName?.givenName ?? "")
+                                    let email = appleIDCredential.email
+                                    let token = String(data: appleIDCredential.identityToken!, encoding: .utf8)
+                                    let AuthorizationCode = String(data: appleIDCredential.authorizationCode!, encoding: .utf8)
+                                    self.isLogin = false
+                                default:
+                                    break
+                                }
+                            case .failure(let error):
+                                print(error.localizedDescription)
+                                print("error")
+                            }
+                        }
+                    )
+                    .frame(width : UIScreen.main.bounds.width * 0.9, height:50)
+                    .cornerRadius(5)
+                    
+                }
+            }
         }
     }
 }
