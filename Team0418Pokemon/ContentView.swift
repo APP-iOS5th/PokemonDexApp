e614ce87b0eef0e50820d80d7a7f9210f87e1b50
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-03 12:54:17 +0900

    Apple Login

--- Team0418Pokemon/ContentView.swift
+++ Team0418Pokemon/ContentView.swift
@@ -10,15 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            AppleLoginView()
         }
-        .padding()
-    }
+        .frame(height: UIScreen.main.bounds.height)
+        .background(Color.white)    }
 }
 
 #Preview {
-    DetailView()
+    ContentView()
 }
